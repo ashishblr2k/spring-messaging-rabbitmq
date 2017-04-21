cec607bafc65a7465b774b73e4b8612b6efa36b9
ashishblr2k <ashishblr2k@gmail.com>
2017-04-21 07:49:54 +0530

    first commit

---
+++ src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,36 @@
+package hello;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by agup101 on 4/20/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @MockBean
+    private Runner runner;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private Receiver receiver;
+
+    @Test
+    public void test() throws Exception {
+        rabbitTemplate.convertAndSend(Application.queueName, "Hello from RabbitMQ!");
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+
+}
